2c14ab0215983429c2e314643259f150de669e28
ryanlin <ryan_lin@winmate.com.tw>
2018-06-14 16:44:21 +0800

    add main program.

---
+++ Main/TestTool/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TestTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView_items = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView_items
+            // 
+            this.treeView_items.Location = new System.Drawing.Point(12, 53);
+            this.treeView_items.Name = "treeView_items";
+            this.treeView_items.Size = new System.Drawing.Size(317, 385);
+            this.treeView_items.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "測試項目";
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(245, 12);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(84, 38);
+            this.btn_start.TabIndex = 2;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView_items);
+            this.Name = "Form1";
+            this.Text = "Test Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView_items;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_start;
+    }
+}
+
---
+++ Main/TestTool/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+namespace TestTool
+{
+    public partial class Form1 : Form
+    {
+        dynamic jsobj = null;
+        List<Thread> threads = new List<Thread>();
+        Thread thread_test;
+        JObject js_result = new JObject();
+        List<string> list_strItems = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private TreeNode build_node(string key, JObject jobj)
+        {
+            TreeNode node = new TreeNode(key);
+            if (jobj["items"] != null)
+            {
+                foreach (string item in jobj["items"])
+                {
+                    node.Nodes.Add(build_node(item, (JObject)jobj[item]));
+                }
+            }
+            else
+            {
+                list_strItems.Add(key);
+                js_result.Add(key,jobj);
+            }
+            return node;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string filepath = "testitem_config.json";
+            if (!File.Exists(filepath))
+            {
+                MessageBox.Show(filepath + " not found.");
+                return ;
+            }            
+            jsobj = JObject.Parse(File.ReadAllText(filepath));
+            treeView_items.ShowLines = true;
+
+            treeView_items.Nodes.Add("Serial Number : " + jsobj.sno);
+            treeView_items.Nodes.Add("Model : " + jsobj.model);
+
+            foreach (string item in jsobj["items"])
+            {
+                treeView_items.Nodes.Add(build_node(item, (JObject)jsobj[item]));
+            }
+            return;
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            if (btn_start.Text == "Start")
+            {
+                btn_start.Text = "Stop";
+                thread_test = new Thread(new ParameterizedThreadStart(TestThread));
+                thread_test.Start(jsobj);
+
+            }
+            else
+            {
+                btn_start.Text = "Start";
+                if (thread_test.IsAlive)
+                    thread_test.Abort();
+                foreach (Thread t in threads)
+                {
+                    if (t.IsAlive)
+                    {
+                        t.Abort();
+                    }
+                }
+            }
+        }
+
+        public void TestThread(object obj)
+        {
+            JObject jobj = obj as JObject;
+
+            foreach (string item in jobj["manual"]["items"])
+            {
+                createProcess(jobj["manual"][item]);
+            }
+
+            foreach (string item in jobj["automatic"]["items"])
+            {
+                Thread t = new Thread(new ParameterizedThreadStart(MultiThread));
+                threads.Add(t);
+                t.Start(jobj["automatic"][item]);
+            }
+
+            bool isCompleted = false;
+            while(!isCompleted)
+            {
+                isCompleted = true;
+                foreach(string item in list_strItems)
+                {
+                    if (js_result[item]["result"]==null)
+                    {
+                        isCompleted = false;
+                    }
+                }
+            }
+            btn_start.Text = "Start";
+        }
+
+        public void MultiThread(object obj)
+        {
+            JObject jobj = obj as JObject;
+            if (jobj["items"] == null)
+            {
+                createProcess(jobj);
+            }
+            else
+            {
+                foreach (string item in jobj["items"])
+                {
+                    MultiThread(jobj[item]);
+                }
+            }
+        }
+
+        public void createProcess(object obj)
+        {
+            //Process.Start(strFile);
+            int i = 0;
+            JObject jobj = obj as JObject;
+            while (true)
+            {
+                Console.WriteLine((string)jobj["name"] + i.ToString());
+                Thread.Sleep(1000);
+                i++;
+                if (i > 10) break;
+            }
+        }
+    }    
+}

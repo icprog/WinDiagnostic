035a6aa0e838e2607243cb511dac907e1feca339
ryanlin <ryan_lin@winmate.com.tw>
2018-06-20 16:11:06 +0800

    Add test item sample code. (test_sample folder)

---
+++ test_sample/test_sample/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace test_sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_pass = new System.Windows.Forms.Button();
+            this.btn_fail = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 34);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 374);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btn_pass
+            // 
+            this.btn_pass.Location = new System.Drawing.Point(381, 72);
+            this.btn_pass.Name = "btn_pass";
+            this.btn_pass.Size = new System.Drawing.Size(169, 63);
+            this.btn_pass.TabIndex = 1;
+            this.btn_pass.Text = "PASS";
+            this.btn_pass.UseVisualStyleBackColor = true;
+            this.btn_pass.Click += new System.EventHandler(this.btn_pass_Click);
+            // 
+            // btn_fail
+            // 
+            this.btn_fail.Location = new System.Drawing.Point(381, 190);
+            this.btn_fail.Name = "btn_fail";
+            this.btn_fail.Size = new System.Drawing.Size(169, 63);
+            this.btn_fail.TabIndex = 2;
+            this.btn_fail.Text = "FAIL";
+            this.btn_fail.UseVisualStyleBackColor = true;
+            this.btn_fail.Click += new System.EventHandler(this.btn_fail_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_fail);
+            this.Controls.Add(this.btn_pass);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_pass;
+        private System.Windows.Forms.Button btn_fail;
+    }
+}
+
---
+++ test_sample/test_sample/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace test_sample
+{
+    public partial class Form1 : Form
+    {
+        string strPath;
+        string strConfigFile;
+        string strResultFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            strPath = Application.StartupPath;
+            strConfigFile = strPath + "\\config.json";
+            strResultFile = strPath + "\\result.json";
+
+            JObject j = JObject.Parse(File.ReadAllText(strConfigFile));
+            textBox1.AppendText(j.ToString());
+        }
+
+        private void btn_pass_Click(object sender, EventArgs e)
+        {
+            JObject j = new JObject();
+            j.Add("result", true);
+            File.WriteAllText(strResultFile, j.ToString());
+            Close();
+        }
+
+        private void btn_fail_Click(object sender, EventArgs e)
+        {
+            JObject j = new JObject();
+            j.Add("result", false);
+            File.WriteAllText(strResultFile, j.ToString());
+            Close();
+        }
+    }
+}

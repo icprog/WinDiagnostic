577dc0e81b917880a49017b31a7c99aa135b8245
ryanlin <ryan_lin@winmate.com.tw>
2018-06-21 15:01:54 +0800

    Move the test item project into TestTool solution. Add release.bat batch file to build a WinDiagnostic release folder automatically.

---
+++ release.bat
@@ -0,0 +1,17 @@
+mkdir Windiagnostic
+for /f "delims=" %%i in ('"dir/a:d/b"') do (
+mkdir Windiagnostic\%%i
+copy %%i\%%i\bin\Release\%%i.exe  Windiagnostic\%%i
+copy %%i\%%i\bin\Release\*.dll  Windiagnostic\%%i
+)
+rmdir Windiagnostic\.git
+rmdir Windiagnostic\sample
+rmdir Windiagnostic\Main
+rmdir Windiagnostic\Windiagnostic
+copy Main\TestTool\bin\Release\Windiagnostic.exe Windiagnostic\
+copy Main\TestTool\bin\Release\*.dll Windiagnostic\
+copy sample\*.json Windiagnostic\
+for /f "delims=" %%i in ('"dir/a:d/b sample"') do (
+mkdir Windiagnostic\%%i
+copy sample\%%i\*.json  Windiagnostic\%%i
+)

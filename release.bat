e1292c90d984936846b7ec0a1cda41da1ca26f0f
Peterson <peterson_liu@winmate.com.tw>
2018-07-04 14:56:15 +0800

    Audio test first version
    Signed-off-by: Peterson <peterson_liu@winmate.com.tw>

--- release.bat
+++ release.bat
@@ -3,6 +3,7 @@ for /f "delims=" %%i in ('"dir/a:d/b"') do (
 mkdir Windiagnostic\%%i
 copy %%i\%%i\bin\Release\%%i.exe  Windiagnostic\%%i
 copy %%i\%%i\bin\Release\*.dll  Windiagnostic\%%i
+copy %%i\%%i\bin\Release\*.wav  Windiagnostic\%%i
 )
 rmdir Windiagnostic\.git
 rmdir Windiagnostic\sample

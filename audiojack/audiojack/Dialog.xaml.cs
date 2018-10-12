76b34584d48514b5398f56a661b40a6e11093361
Peterson <peterson_liu@winmate.com.tw>
2018-10-12 16:12:10 +0800

    Add audiojack and global style in functionbutton
    Signed-off-by: Peterson <peterson_liu@winmate.com.tw>

---
+++ audiojack/audiojack/Dialog.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace audiojack
+{
+    /// <summary>
+    /// Interaction logic for Dialog.xaml
+    /// </summary>
+    /// 
+    public partial class Dialog : Window
+    {
+        #region Audio DLL Import
+        [DllImport("winmm.DLL", EntryPoint = "PlaySound", SetLastError = true, CharSet = CharSet.Unicode, ThrowOnUnmappableChar = true)]
+        // The PlaySound method supports the playing of normal wave files stored on the disk.
+        private static extern bool PlaySound(string pszSound, int hmod, int fdwSound);
+        [DllImport("winmm.DLL")]
+        public static extern int waveOutSetVolume(IntPtr hwo, uint dwVolume);
+
+        [System.Flags]
+        public enum PlaySoundFlags : int
+        {
+            SND_SYNC = 0x0000,/* play synchronously (default) */
+            SND_ASYNC = 0x0001, /* play asynchronously */
+            SND_NODEFAULT = 0x0002, /* silence (!default) if sound not found */
+            SND_MEMORY = 0x0004, /* pszSound points to a memory file */
+            SND_LOOP = 0x0008, /* loop the sound until next sndPlaySound */
+            SND_NOSTOP = 0x0010, /* don't stop any currently playing sound */
+            SND_NOWAIT = 0x00002000, /* don't wait if the driver is busy */
+            SND_ALIAS = 0x00010000,/* name is a registry alias */
+            SND_ALIAS_ID = 0x00110000, /* alias is a pre d ID */
+            SND_FILENAME = 0x00020000, /* name is file name */
+            SND_RESOURCE = 0x00040004, /* name is resource name or atom */
+            SND_PURGE = 0x0040,  /* purge non-static events for task */
+            SND_APPLICATION = 0x0080 /* look for application specific association */
+        }
+        #endregion
+
+        string AudioFileLocation = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Left1.wav"); // 測試音效的預設路徑
+        string _FileName = "";
+        string _AudioResult = "";
+
+        public string AudioResult
+        {
+            get
+            {
+                return _AudioResult;
+            }
+        }
+
+        public Dialog(string DataFromUserControl)
+        {
+            InitializeComponent();
+            _FileName = DataFromUserControl;
+            Loaded += dialog_Load;
+        }
+
+        private void dialog_Load(object sender, RoutedEventArgs routedEventArgs)
+        {
+            AudioFileLocation = System.AppDomain.CurrentDomain.BaseDirectory + "\\" + _FileName + ".wav";
+
+            if (File.Exists(AudioFileLocation))
+            {
+                if (_FileName.Contains("Right")) waveOutSetVolume(IntPtr.Zero, 0xFFFF0000);
+                else waveOutSetVolume(IntPtr.Zero, 0x0000FFFF);
+                PlaySound(AudioFileLocation, 0, (int)(PlaySoundFlags.SND_ASYNC | PlaySoundFlags.SND_FILENAME)); // 調用API播放聲音
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Audio File does not exist.", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                _AudioResult = "FAIL";
+                this.DialogResult = true;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.Button button = sender as System.Windows.Controls.Button;
+            Console.WriteLine("dialog button name: {0}",button.Name);
+            Console.WriteLine("_FileName: {0}", _FileName);
+            if (button != null)
+            {
+                Regex regex = new Regex(@"\d+");
+                var ButtonMatch = regex.Match(button.Name);
+                var FileNameMatch = regex.Match(_FileName);
+
+                if(ButtonMatch.Success && FileNameMatch.Success)
+                {
+                    if (string.Equals(ButtonMatch.Value,FileNameMatch.Value))
+                    {
+                        _AudioResult = "PASS";
+                    }
+                    else
+                    {
+                        _AudioResult = "FAIL";
+                    }
+                    Trace.WriteLine("Audio Select Button : " + button.Name + " , Result : " + _AudioResult);
+                }
+                else
+                {
+                    throw new Exception();
+                }                
+            }
+            waveOutSetVolume(IntPtr.Zero, 0xFFFFFFFF); // 修正單聲道測試時, 左右聲道都有聲音
+            this.DialogResult = true;
+        }
+    }
+}
---
+++ audiojack/audiojack/MainWindow.xaml.cs
@@ -0,0 +1,236 @@
+﻿using Newtonsoft.Json.Linq;
+using SpeakerMicAutoTestApi;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace audiojack
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int mRandom = 0;
+        int SelectedAudioName = 0;
+        uint WaitTimeCount = 0;
+        uint WaitTime = 2;
+        uint CountdownForUI = 0;
+        bool IsAutoModeAudioJack = true;
+        JObject result = new JObject();
+        System.Windows.Forms.Timer timer;
+        AudioTest AutoModeAudioTest = null;
+        string TestProduct = string.Empty;
+        double ExternalRecordThreshold = 0.0;
+        double InternalRecordThreshold = 0.0;
+        double AudioJackRecordThreshold = 0.0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += audiojack_Load;
+        }
+
+        private void audiojack_Load(object sender, RoutedEventArgs routedEventArgs)
+        {
+            result["result"] = "FAIL";
+            var jsonconfig = GetFullPath("config.json");
+            if (!File.Exists(jsonconfig))
+            {
+                MessageBox.Show("config.json not founded");
+                Application.Current.Shutdown();
+            }
+
+            dynamic jobject = JObject.Parse(File.ReadAllText(jsonconfig));
+            TestProduct = jobject.TestProduct.ToString();
+            ExternalRecordThreshold = (double)jobject.ExternalRecordThreshold;
+            InternalRecordThreshold = (double)jobject.InternalRecordThreshold;
+            AudioJackRecordThreshold = (double)jobject.AudioJackRecordThreshold;
+            IsAutoModeAudioJack = (bool)jobject.IsAutoModeAudioJack;
+
+            Trace.WriteLine("AudioJack_Load");
+
+            if (IsAutoModeAudioJack)
+            {
+                timer = new System.Windows.Forms.Timer();
+                //設定計時器的速度
+                timer.Interval = 750;
+                timer.Tick += new EventHandler(TimerAutoTestAudioJack_Tick);
+                timer.Start();
+            }
+            else
+            {
+                ManualMode.Visibility = Visibility.Visible;
+                mRandom = 0;
+            }
+        }
+
+        private void TimerAutoTestAudioJack_Tick(object sender, EventArgs e)
+        {
+            if (WaitTimeCount < WaitTime)
+            {
+                Title.Content = "Wait " + (WaitTime - CountdownForUI) + " sec for Test";
+                Title.Foreground = Brushes.Orange;
+                Title.UpdateLayout();
+            }
+            if (WaitTimeCount.Equals(WaitTime))
+            {
+                timer.Stop();
+                Title.Content = "AudioJack";
+                Title.Foreground = Brushes.Black;
+                Title.UpdateLayout();
+                checkTestStatus(AutoModeAudioTestFunction());
+            }
+
+            WaitTimeCount++;
+            CountdownForUI++;
+        }
+
+        string AutoModeAudioTestFunction()
+        {
+            try
+            {
+                if (AutoModeAudioTest == null) AutoModeAudioTest = new AudioTest(TestProduct, true);
+
+                string wavFile = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "WinmateAudioTest.wav");
+                if (File.Exists(wavFile))
+                {
+                    AutoModeAudioTest.WavFileName = wavFile;
+
+                    AutoModeAudioTest.ExternalRecordThreshold = ExternalRecordThreshold;
+                    AutoModeAudioTest.InternalRecordThreshold = InternalRecordThreshold;
+                    AutoModeAudioTest.AudioJackRecordThreshold = AudioJackRecordThreshold;
+
+                    Trace.WriteLine("AudioJack Test");
+                    AutoModeAudioTest.AudioJackTest();
+                    Trace.WriteLine("AudioJackIntensity : " + AutoModeAudioTest.AudioJackIntensity);
+                    Trace.WriteLine("AudioJackRecordThreshold : " + AudioJackRecordThreshold);
+
+                    if (IsIntensityCorrect())
+                    {
+                        return "PASS";
+                    }
+                    else
+                    {
+                        return "Intensity does not meet the Threshold.";
+                    }
+                }
+                else
+                {
+                    Trace.WriteLine("Audio test file (" + wavFile + ") does not exist.");
+                    return "Audio test file does not exist";
+                }
+            }
+            catch (Exception ex)
+            {
+                if (AutoModeAudioTest != null) AutoModeAudioTest = null;
+                return "(AudioAutoTest) " + ex.Message;
+            }
+        }
+
+        bool IsIntensityCorrect()
+        {
+            return (AutoModeAudioTest.AudioJackIntensity > AudioJackRecordThreshold) && !AutoModeAudioTest.AudioJackIntensity.Equals(-1);
+        }
+
+        string GetFullPath(string path)
+        {
+            var exepath = System.AppDomain.CurrentDomain.BaseDirectory;
+            return System.IO.Path.Combine(exepath, path);
+        }
+
+        int MakeRand(int intLower, int intUpper)
+        {
+            Random random = new Random((int)DateTime.Now.Ticks);
+            return random.Next(intLower, intUpper + 1);
+        }
+
+        public Dialog PlayTestAudio(Button button)
+        {
+            string AudioFileName = "";
+
+            mRandom = MakeRand(1, 5);
+            Trace.WriteLine("New Random: " + mRandom);
+
+            if (button.Name.Contains("Left")) AudioFileName = "Left" + mRandom.ToString();
+            else AudioFileName = "Right" + mRandom.ToString();
+            Trace.WriteLine("AudioFileName = " + AudioFileName);
+
+            Dialog mAudioSelect = new Dialog(AudioFileName);
+            return mAudioSelect;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            Console.WriteLine(button.Name);
+            if (button != null)
+            {
+                Dialog mAudioSelect = PlayTestAudio(button);
+                if ((bool)mAudioSelect.ShowDialog())
+                {
+                    //    // 從Form(mAudioSelect)取值並顯示到UserControl(Audio), 並判斷是否測試檔有無選擇正確, 如果選擇錯誤直接給FAIL
+                    if (mAudioSelect.AudioResult.Equals("FAIL"))
+                    {
+                        Fail.RaiseEvent(e);
+                    }
+                    else // 當測試ok, 才允許測試下一個聲道
+                    {
+                        switch (button.Name)
+                        {
+                            case "Left":
+                                Left.IsEnabled = false;
+                                Right.IsEnabled = true;
+                                break;
+                            case "Right":
+                                Right.IsEnabled = false;
+                                checkTestStatus("PASS");
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void checkTestStatus(String testResult)
+        {
+            mRandom = 0;
+            SelectedAudioName = 0;
+
+            if (testResult.Equals("PASS"))
+            {
+                Console.WriteLine("PASS");
+                result["result"] = "PASS";
+            }
+            else
+            {
+                result["result"] = "FAIL";
+                Console.WriteLine("FAIL");
+                Left.IsEnabled = true;
+                Right.IsEnabled = false;
+            }
+
+            File.WriteAllText(GetFullPath("result.json"), result.ToString());
+            Thread.Sleep(200);
+            File.Create(GetFullPath("completed"));
+            Application.Current.Dispatcher.BeginInvokeShutdown(System.Windows.Threading.DispatcherPriority.Send);
+        }
+
+        private void Fail_Click(object sender, RoutedEventArgs e)
+        {
+            checkTestStatus("FAIL");
+        }
+    }
+}
